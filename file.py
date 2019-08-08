c83cfbcfd7144c096c1ccdb4a9d7d7b18bc46b2d
himansh-23 <himanshu00096@gmail.com>
2019-08-08 15:27:55 +0530

    rebase method call

--- file.py
+++ file.py
@@ -4,11 +4,8 @@ print(5+3)
 
 def substract(a,b):
   return a-b;
-<<<<<<< 0d4ad4360517ef0336643dfa8be4a372a724747c
 
 print(substract(3,5))
 
-=======
   
 print(substract(5,3))
->>>>>>> add method call
